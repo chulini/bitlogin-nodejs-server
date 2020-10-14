fe7fafd7081eeb38316cf62eb2a8c35e9bf08a01
JQ <chulini@gmail.com>
2020-10-14 20:45:53 +0200

    no innecesary logs

--- bitlogin/SignatureVerifier.js
+++ bitlogin/SignatureVerifier.js
@@ -2,9 +2,6 @@ const bitcoinMessage = require('bitcoinjs-message');
 
 module.exports = class SignatureVerifier {
     static VerifySignature(legacyAddress, message, signature) {
-        console.log(`\nlegacyAddress:\n${legacyAddress}`)
-        console.log(`\nmessage:\n${message}`)
-        console.log(`\nsignature\n${signature}`)
         try {
             let verified = false;
             verified = bitcoinMessage.verify(message, legacyAddress, signature);
